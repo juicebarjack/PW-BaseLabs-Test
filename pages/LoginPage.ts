0e1304389e5c864ef3230b6fd9154a2d24b2e52e
juicebarjack <info@juicebarjack.com>
2026-07-24 17:46:28 -0400

    fixed issue with url, added parameters for login button instead of /login

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,25 +2,40 @@ import { test, Page, expect, Locator } from "@playwright/test";
 
 export class LoginPage { 
     readonly page: Page;
+    readonly loginButton: Locator;
     readonly userNameInput: Locator;
     readonly passWordInput: Locator;
+    readonly submitLoginButton: Locator;
+    readonly logoutButton: Locator;
 
     constructor (page: Page) {
         this.page = page;
         this.userNameInput = page.locator('#username');
         this.passWordInput = page.locator('#password');
+        this.loginButton = page.getByRole('link', { name: 'Login' });
+        this.logoutButton = page.getByRole('link', { name: 'Logout' });
+        this.submitLoginButton = page.getByRole('button', { name: 'Login' });
     }
 
     async goTo(){
-        await this.page.goto('/login', {
+        await this.page.goto( process.env.BASE_URL!, {
             waitUntil: 'domcontentloaded'
         });
     }
+    
+    async login(){
+        await this.loginButton.click();
+    }
+
     async fillForm(
         userName: string = process.env.USERNAME!,
         passWord: string = process.env.PASSWORD!
     ){
         await this.userNameInput.fill(userName); 
         await this.passWordInput.fill(passWord);
+    }
+
+    async submit() {
+        await this.submitLoginButton.click();
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,14 +2,18 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 
 test('Successful Login', async ({ page }) => {
-  await page.goto('/login');
+  await page.goto(process.env.BASE_URL!);
   await page.locator('#username').fill(process.env.USERNAME!);
   await page.locator('#password').fill(process.env.PASSWORD!);
   await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
 });
 
 test('Login with POM', async ({ page }) => {
   const loginPage = new LoginPage(page);
-  loginPage.goTo();
+  await loginPage.goTo();
+  await loginPage.login();
   await loginPage.fillForm();
+  await loginPage.submit();
+  await expect(loginPage.logoutButton).toBeVisible();
 });
